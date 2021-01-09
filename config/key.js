ebc3df228c533fe15ae59297b3c87a83aca84526
CameronLumpkinATL <camlump@yahoo.com>
2021-01-08 23:50:37 -0500

    changed protected information

--- config/key.js
+++ config/key.js
@@ -4,7 +4,3 @@ if (process.env.NODE_ENV === 'Production') {
     module.exports = require('./dev')
 }
 
-// module.exports = {
-//     mongoURI:'mongodb+srv://camlump:12345@cluster0.vo7mu.mongodb.net/image-data?retryWrites=true&w=majority',
-//     secretOrKey: "secret"
-// };
